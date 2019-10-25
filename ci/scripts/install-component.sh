37754fbebb58e70215de26113225eea38ee8e346
Taiki Endo <te316e89@gmail.com>
2019-10-25 11:40:08 +0900

    ci: refactor scripts

--- ci/scripts/install-component.sh
+++ ci/scripts/install-component.sh
@@ -3,18 +3,18 @@
 set -euo pipefail
 
 set +e
-if rustup component add $RUST_COMPONENT; then
+if rustup component add "$1"; then
     set -e
 else
     set -e
     # If the component is unavailable on the latest nightly,
     # use the latest toolchain with the component available.
     # Refs: https://github.com/rust-lang/rustup-components-history#the-web-part
-    target=$(curl -sSf https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/$RUST_COMPONENT)
-    echo "'$RUST_COMPONENT' is unavailable on the toolchain '$RUST_TOOLCHAIN', use the toolchain 'nightly-$target' instead"
-    rustup toolchain install nightly-$target --no-self-update
-    rustup default nightly-$target
-    rustup component add $RUST_COMPONENT
+    target=$(curl -sSf https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/$1)
+    echo "'$1' is unavailable on the toolchain '${RUST_TOOLCHAIN}', use the toolchain 'nightly-${target}' instead"
+    rustup toolchain install "nightly-${target}" --no-self-update
+    rustup default "nightly-${target}"
+    rustup component add "$1"
 
     echo "Query rust and cargo versions:"
     rustup -V
@@ -23,7 +23,7 @@ else
 fi
 
 echo "Query component versions:"
-case $RUST_COMPONENT in
+case "$1" in
     clippy) cargo clippy -V ;;
     rustfmt) rustfmt -V ;;
 esac
--- ci/scripts/install-shfmt.sh
+++ ci/scripts/install-shfmt.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 go get -u github.com/mvdan/sh/cmd/shfmt
-export PATH=$PATH:$HOME/go/bin
-echo "##[add-path]$HOME/go/bin"
+export PATH=${PATH}:${HOME}/go/bin
+echo "##[add-path]${HOME}/go/bin"
 
 echo "Query shfmt versions:"
 shfmt --version
