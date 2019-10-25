3f47aa365ffb614f35dc574968fb4a7ae734bd05
taiki-e <te316e89@gmail.com>
2019-10-25 05:37:22 +0000

    ci: refactor scripts (#10)

--- ci/scripts/install-rust.sh
+++ ci/scripts/install-rust.sh
@@ -3,14 +3,14 @@
 set -euo pipefail
 
 if [[ "${AGENT_OS}" == "macOS-latest" ]]; then
-    curl -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain $RUST_TOOLCHAIN
-    export PATH=$PATH:$HOME/.cargo/bin
-    echo "##[add-path]$HOME/.cargo/bin"
+    curl -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain "${RUST_TOOLCHAIN}"
+    export PATH=${PATH}:${HOME}/.cargo/bin
+    echo "##[add-path]${HOME}/.cargo/bin"
 else
     # TODO: when default rustup is bumped to 1.20+, enable this.
     # rustup set profile minimal
-    rustup toolchain install $RUST_TOOLCHAIN --no-self-update
-    rustup default $RUST_TOOLCHAIN
+    rustup toolchain install "${RUST_TOOLCHAIN}" --no-self-update
+    rustup default "${RUST_TOOLCHAIN}"
 fi
 
 echo "Query rust and cargo versions:"
