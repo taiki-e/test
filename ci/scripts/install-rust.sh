ed852f4c480bb6bf64861214e0848f084644df60
Taiki Endo <te316e89@gmail.com>
2019-11-06 04:19:59 +0900

    ci: clean up install scripts

--- ci/scripts/install-rust.sh
+++ ci/scripts/install-rust.sh
@@ -2,18 +2,20 @@
 
 set -euo pipefail
 
-case "${AGENT_OS:-ubuntu-latest}" in
-    macos-*)
-        curl -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain "${RUST_TOOLCHAIN:-nightly}"
-        export PATH=${PATH}:${HOME}/.cargo/bin
-        echo "##[add-path]${HOME}/.cargo/bin"
-        ;;
-    ubuntu-* | windows-*)
-        rustup set profile minimal
-        rustup update "${RUST_TOOLCHAIN:-nightly}" --no-self-update
-        rustup default "${RUST_TOOLCHAIN:-nightly}"
-        ;;
-esac
+toolchain="${1:-nightly}"
+
+set +e
+if rustup -V 2>/dev/null; then
+    set -e
+    rustup set profile minimal
+    rustup update "${toolchain}" --no-self-update
+    rustup default "${toolchain}"
+else
+    set -e
+    curl -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain "${toolchain}"
+    export PATH=${PATH}:${HOME}/.cargo/bin
+    echo "##[add-path]${HOME}/.cargo/bin"
+fi
 
 echo "Query rust and cargo versions:"
 rustup -V
