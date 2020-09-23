7477957d98c245aff8c5c5576516c783b0d87ef5
Taiki Endo <te316e89@gmail.com>
2020-09-24 05:25:46 +0900

    Update CI config

--- ci/clippy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-. ci/install-component.sh clippy
-
-cargo clippy --all --all-features --all-targets
--- ci/rustfmt.sh
+++ ci/rustfmt.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Format all rust code with `rustfmt`.
+#
+# Usage:
+#
+#    bash ci/rustfmt.sh
+#
+# To print a diff and exit 1 if code is not formatted, but without changing any
+# files, use:
+#
+#    bash ci/rustfmt.sh check
+#
+
 set -euo pipefail
 
-. ci/install-component.sh rustfmt
-
-if [[ "${CI:-false}" == "true" ]]; then
+if [[ "${1:-fmt}" == "check" ]]; then
     rustfmt --check --edition 2018 "$(find . -name '*.rs' -print)"
 else
     rustfmt --edition 2018 "$(find . -name '*.rs' -print)"
