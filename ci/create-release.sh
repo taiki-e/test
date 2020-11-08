0e8ba1aadeb4b6b6cf60e27ccc3bab87b82070fa
Taiki Endo <te316e89@gmail.com>
2020-11-08 22:34:22 +0900

    create-version.sh: remove support for package name

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
+# Create a new GitHub release.
+#
+# Note:
+# - The generated note format is:
+#   "See the [release notes]($LINK_TO_CHANGELOG) for a complete list of changes."
+# - The generated link format is:
+#   "https://github.com/$GITHUB_REPOSITORY/blob/HEAD/CHANGELOG.md#${TAG/v/}---${RELEASE_DATE}"
+# - This script assumes that the format (file name and title of section) of
+#   release notes is based on Keep a Changelog (https://keepachangelog.com/en/1.0.0).
+# - The valid tag format is "vMAJOR.MINOR.PATCH(-PRERELEASE)(+BUILD_METADATA)"
+#   See also Semantic Versioning (https://semver.org)
+# - The release date is based on the time this script was run, the time zone is
+#   the UTC.
+# - The generated link to the release notes will be broken when the version
+#   yanked if the project adheres to the Keep a Changelog's yanking style.
+#   Consider adding a note like the following instead of using the "[YANKED]" tag:
+#   "**Note: This version was yanked. See $LINK_TO_YANKED_REASON for details.**"
+
 set -euo pipefail
 IFS=$'\n\t'
 
 ref="${GITHUB_REF:?}"
-tag=${ref#*/tags/}
-repo="${GITHUB_REPOSITORY:?}"
+tag="${ref#*/tags/}"
 
-# valid tag format: (PACKAGE_NAME-)vMAJOR.MINOR.PATCH(-PRERELEASE)(+BUILD_METADATA)
-# Refs: https://semver.org
-if [[ ! "${tag}" =~ (^|^[a-zA-Z_0-9-]+-)v[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z_0-9\.-]+)?(\+[a-zA-Z_0-9\.-]+)?$ ]]; then
-  echo "invalid tag format: ${tag}"
+if [[ ! "${tag}" =~ v[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z_0-9\.-]+)?(\+[a-zA-Z_0-9\.-]+)?$ ]]; then
+  echo "error: invalid tag format: ${tag}"
   exit 1
-elif [[ "${tag}" =~ (^|^[a-zA-Z_0-9-]+-)v[0-9\.]+-[a-zA-Z_0-9\.-]+(\+[a-zA-Z_0-9\.-]+)?$ ]]; then
+elif [[ "${tag}" =~ v[0-9\.]+-[a-zA-Z_0-9\.-]+(\+[a-zA-Z_0-9\.-]+)?$ ]]; then
   prerelease="--prerelease"
 fi
-package_name=$(echo "${tag}" | sed -r "s/-v[0-9\.]+(-[a-zA-Z_0-9\.-]+)?$//")
-version=$(echo "${tag}" | sed -r "s/(^|^[a-zA-Z_0-9-]+-)v//")
+version="${tag/v/}"
 date=$(date --utc '+%Y-%m-%d')
-if [[ "${package_name}" == "${tag}" ]]; then
-  title="${version}"
-else
-  title="${package_name} ${version}"
-  repo="${repo}/${package_name}"
-fi
-# TODO: this link will be broken when the version yanked if the project adheres to the keep-a-changelog's yanking style.
-changelog="https://github.com/${repo}/blob/HEAD/CHANGELOG.md#${version//./}---${date}"
+title="${version}"
+changelog="https://github.com/${GITHUB_REPOSITORY:?}/blob/HEAD/CHANGELOG.md#${version//./}---${date}"
 notes="See the [release notes](${changelog}) for a complete list of changes."
 
 if [[ -z "${GITHUB_TOKEN:-}" ]]; then
