32ad051c6dce851f4ca84375a9742cb619bc57b9
Taiki Endo <te316e89@gmail.com>
2021-03-02 21:58:56 +0900

    Remove rustversion dependency

--- rust/lint/tests/test.rs
+++ rust/lint/tests/test.rs
@@ -2,7 +2,6 @@ use std::{env, fs, path::Path, process::Command, str};
 
 use tempfile::Builder;
 
-#[rustversion::attr(before(2021-02-19), ignore)] // Note: This date is commit-date and the day before the toolchain date.
 #[test]
 fn check_lint_list() {
     let rustc = env::var_os("RUSTC").unwrap_or_else(|| "rustc".into());
