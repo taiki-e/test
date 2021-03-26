1080c9e08db63818e5eb0343e77136ea6e88b970
Taiki Endo <te316e89@gmail.com>
2021-03-27 03:28:30 +0900

    Update assert_diff

--- rust/lint/tests/test.rs
+++ rust/lint/tests/test.rs
@@ -15,24 +15,21 @@ fn assert_diff(expected_path: impl AsRef<Path>, actual: impl AsRef<str>) {
     let actual = actual.as_ref();
     let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
     let expected_path = &manifest_dir.join(expected_path);
-    (|| -> Result<(), Box<dyn std::error::Error>> {
-        let expected = fs::read_to_string(expected_path)?;
-        if expected != actual {
-            if env::var_os("CI").is_some() {
-                let outdir = Builder::new().prefix("assert_diff").tempdir()?;
-                let actual_path = &outdir.path().join(expected_path.file_name().unwrap());
-                fs::write(actual_path, actual)?;
-                let status = Command::new("git")
-                    .args(&["--no-pager", "diff", "--no-index", "--"])
-                    .args(&[expected_path, actual_path])
-                    .status()?;
-                assert!(!status.success());
-                panic!("assertion failed");
-            } else {
-                fs::write(expected_path, actual)?;
-            }
+    let expected = fs::read_to_string(expected_path).unwrap();
+    if expected != actual {
+        if env::var_os("CI").is_some() {
+            let outdir = Builder::new().prefix("assert_diff").tempdir().unwrap();
+            let actual_path = &outdir.path().join(expected_path.file_name().unwrap());
+            fs::write(actual_path, actual).unwrap();
+            let status = Command::new("git")
+                .args(&["--no-pager", "diff", "--no-index", "--"])
+                .args(&[expected_path, actual_path])
+                .status()
+                .unwrap();
+            assert!(!status.success());
+            panic!("assertion failed");
+        } else {
+            fs::write(expected_path, actual).unwrap();
         }
-        Ok(())
-    })()
-    .unwrap_or_else(|e| panic!("{}", e))
+    }
 }
