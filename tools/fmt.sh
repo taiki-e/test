8882e3ca6908a48d81196e71e1f019c6550b2198
Taiki Endo <te316e89@gmail.com>
2021-05-12 00:46:16 +0900

    Update tools/fmt.sh

--- tools/fmt.sh
+++ tools/fmt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Format shell scripts and YAML files.
+# Format all code.
 #
 # Usage:
 #    ./tools/fmt.sh
 #
-# Note: This script requires shfmt and prettier.
+# Note: This script requires rustfmt, shfmt, and prettier.
 
 set -euo pipefail
 IFS=$'\n\t'
@@ -14,15 +14,17 @@ cd "$(cd "$(dirname "${0}")" && pwd)"/..
 
 # shellcheck disable=SC2046
 if [[ -z "${CI:-}" ]]; then
-    (
-        set -x
-        shfmt -l -w $(git ls-files "*.sh")
-        prettier -l -w $(git ls-files "*.yml")
-    )
+    # `cargo fmt` cannot recognize modules defined inside macros, so run
+    # rustfmt directly.
+    # Refs: https://github.com/rust-lang/rustfmt/issues/4078
+    rustfmt $(git ls-files '*.rs')
+    shfmt -l -w $(git ls-files '*.sh')
+    prettier -l -w $(git ls-files '*.yml')
 else
-    (
-        set -x
-        shfmt -d $(git ls-files "*.sh")
-        prettier -c $(git ls-files "*.yml")
-    )
+    # `cargo fmt` cannot recognize modules defined inside macros, so run
+    # rustfmt directly.
+    # Refs: https://github.com/rust-lang/rustfmt/issues/4078
+    rustfmt --check $(git ls-files '*.rs')
+    shfmt -d $(git ls-files '*.sh')
+    prettier -c $(git ls-files '*.yml')
 fi
