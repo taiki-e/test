7446796be518d70cd2d191b071a6ca5a97fcc94d
Taiki Endo <te316e89@gmail.com>
2021-05-23 15:45:49 +0900

    commit target-spec-json

---
+++ tools/gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run code generators.
+#
+# Usage:
+#    ./tools/gen.sh
+
+set -euo pipefail
+IFS=$'\n\t'
+
+cd "$(cd "$(dirname "${0}")" && pwd)"/..
+
+./tools/target-spec-json.sh
---
+++ tools/target-spec-json.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update built-in targets list.
+#
+# Usage:
+#    ./tools/target-spec-json.sh
+#
+# https://doc.rust-lang.org/nightly/rustc/platform-support.html
+
+set -euo pipefail
+IFS=$'\n\t'
+
+cd "$(cd "$(dirname "${0}")" && pwd)"/..
+
+for target in $(rustc --print target-list); do
+    rustc --print target-spec-json -Z unstable-options --target "${target}" \
+        >./rust/target-spec-json/"${target}".json
+done
