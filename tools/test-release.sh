ce938784c43551035dfda42f50a07f99e86a8400
Taiki Endo <te316e89@gmail.com>
2022-09-09 00:41:30 +0900

    test

--- tools/test-release.sh
+++ tools/test-release.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-tag="v0.0.1-pre_invalid"
-# tag="v0.0.2+metadata_invalid"
+tag="v0.0.1"
 git tag -d "${tag}" || true
 gh release delete "${tag}" -y || true
 git push --delete origin "${tag}" || true
