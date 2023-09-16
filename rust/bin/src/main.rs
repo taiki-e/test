c49cb1d3377c88b21d5916b0d3963f0539917f52
Taiki Endo <te316e89@gmail.com>
2023-09-16 19:50:29 +0900

    tidy: Update tools/tidy.sh and add SPDX-License-Identifier

--- rust/bin/src/main.rs
+++ rust/bin/src/main.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 fn main() {
     #[cfg(target_os = "macos")]
     println!("macos");
--- rust/lint/src/main.rs
+++ rust/lint/src/main.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 use std::{env, fs, path::Path, process::Command, str};
 
 fn main() {
