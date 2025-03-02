dfe9135246846210bb5f9634763eadfbf9310da8
Taiki Endo <te316e89@gmail.com>
2025-03-02 21:01:13 +0900

    Update scripts

--- tools/test-release.sh
+++ tools/test-release.sh
@@ -6,8 +6,6 @@ trap -- 's=$?; printf >&2 "%s\n" "${0##*/}:${LINENO}: \`${BASH_COMMAND}\` exit w
 cd -- "$(dirname -- "$0")"/..
 
 tag="${1:-v0.0.0}"
-git tag -d "${tag}" || true
 gh release delete "${tag}" -y || true
-git push --delete origin "${tag}" || true
-git tag "${tag}"
-git push origin --tags
+git tag -f "${tag}"
+git push origin -f refs/tags/"${tag}"
