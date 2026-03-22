3bfd80d1fd5ecb189e325927c4d7fedba0a33226
Taiki Endo <te316e89@gmail.com>
2026-03-22 16:29:36 +0900

    ci: Update CI config

--- tools/test-release.sh
+++ tools/test-release.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 trap -- 's=$?; printf >&2 "%s\n" "${0##*/}:${LINENO}: \`${BASH_COMMAND}\` exit with ${s}"; exit ${s}' ERR
 cd -- "$(dirname -- "$0")"/..
 
-tag="${1:-v0.0.0}"
+tag="${1:-v0.0.1}"
 gh release delete "${tag}" -y || true
 git tag -f "${tag}"
 git push origin -f refs/tags/"${tag}"
